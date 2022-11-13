008117c327636d14521a7ed86ea7000574728712
PersonalWebsite2022 <PersonalWebsite2022@gmail.com>
2022-11-13 12:59:00 -0800

    Add project files.

---
+++ ShowResultBackend/Controllers/ResultController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShowResultBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResultController : ControllerBase
+    {
+        //https://localhost:40001/swagger/index.html
+        [HttpGet("ASimpleTask/{result}")]
+        [EnableCors("PolicyAllowAll")]
+        public async Task<IActionResult> ASimpleTaskAsync(bool result)
+        {
+            await Task.Delay(1000);
+
+            if (result)
+            {
+                StatusCode(StatusCodes.Status200OK);
+                return new JsonResult("The task has been done successfully.");
+            }
+            else
+            {
+                StatusCode(StatusCodes.Status500InternalServerError);
+                return new JsonResult("An error occured. Try it later.");
+            }
+        }
+    }
+}
